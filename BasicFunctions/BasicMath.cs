82a6f620ce6e01fdcfe4d88498930fae94782645
MaksR-dev <maksymrudakov@gmail.com>
2023-05-18 18:02:45 +0200

    Commit. Was added second project with basic maths for calculations.

---
+++ BasicFunctions/BasicMath.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicFunctions;
+
+public static class BasicMath
+{
+    public static int Min(int a, int b)
+    {
+        return a < b ? a : b;
+    } 
+    public static int Max(int a, int b)
+    {
+        return a > b ? a : b;
+    } 
+}
